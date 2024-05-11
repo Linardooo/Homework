6972f94e646a76e058081309a739fe31b5c22c18
Linard <linardminsk@gmail,com>
2024-05-11 23:33:58 +0300

    commit #10

--- homework5.py
+++ homework5.py
@@ -1,23 +1,28 @@
 # Задание №1
+
 a = "www.my_site.com#about"
 print(a.replace("#", " / ")
 
 # Задание №2
+
 s = ""
 while s == "":
     s = input()
     print(s + "ing")
 
 # Задание №3
+
 g = "Ivanou Ivan"
 g = g.split(" ")
 print(g[1],g[0])
 
 # Задание №4
+
 text = " multiple statements found "
 print(text.lstrip().rstrip())
 
 # Задание №5
+
 name = "vasya petya kolya dima maksim anton makar"
 print(name.title())
 
