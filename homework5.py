60f62050beb1a949b54027c0b3ce62e46fa7bb83
Linard <linardminsk@gmail,com>
2024-05-12 18:14:09 +0300

    commit #18

--- homework5.py
+++ homework5.py
@@ -1,34 +1,28 @@
-"""Заменить символ “#” на символ “/”
-в строке 'www.my_site.com#about'
-"""
+"""Homework5
+Murahsko Maksim Yurevich
+Course Python authomatic tester"""
 
+# Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
 A = "www.my_site.com#about"
 print(A.replace("#", "/"))
 
-"""Напишите программу, 
-которая добавляет ‘ing’ к словам"""
-
+# Напишите программу, которая добавляет ‘ing’ к словам
 S = ""
 while S == "":
     S = input()
 print(S + "ing")
 
-"""В строке “Ivanou Ivan” поменяйте 
-местами слова => "Ivan Ivanou" """
-
+# В строке “Ivanou Ivan” поменяйте естами слова => "Ivan Ivanou"
 name_lastname = "Ivanou Ivan"
 name_lastname = name_lastname.split(" ")
 print(name_lastname[1], name_lastname[0])
 
-"""Напишите программу которая 
-удаляет пробел в начале, в конце строки"""
-
+# Напишите программу которая удаляет пробел в начале, в конце строки
 text = " multiple statements found "
 print(text.lstrip().rstrip())
 
-"""Имена собственные всегда начинаются с заглавной буквы, 
-за которой следуют строчные буквы. Исправьте данное имя собственное так, 
-чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris" """
-
+# Имена собственные всегда начинаются с заглавной буквы,
+# за которой следуют строчные буквы. Исправьте данное имя собственное так,
+# чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris"
 name = "vasya petya kolya dima maksim anton makar"
 print(name.title())
