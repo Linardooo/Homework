dd9fa157fe956c6c72eaaa3a1ee20afefc4eb535
Linard <linardminsk@gmail,com>
2024-05-12 17:43:02 +0300

    commit #15

--- homework5.py
+++ homework5.py
@@ -1,28 +1,31 @@
-# Задание №1
+"""Заменить символ “#” на символ “/”
+в строке 'www.my_site.com#about'
+"""
 
 a = "www.my_site.com#about"
-print(a.replace("#", " / ")
+print(a.replace("#", "/"))
 
-# Задание №2
+"""Напишите программу, которая добавляет ‘ing’ к словам"""
 
 s = ""
 while s == "":
     s = input()
 print(s + "ing")
 
-# Задание №3
+"""В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou" """
 
 g = "Ivanou Ivan"
 g = g.split(" ")
 print(g[1], g[0])
 
-# Задание №4
+"""Напишите программу которая удаляет пробел в начале, в конце строки"""
 
 text = " multiple statements found "
 print(text.lstrip().rstrip())
 
-# Задание №5
+"""Имена собственные всегда начинаются с заглавной буквы, 
+за которой следуют строчные буквы. Исправьте данное имя собственное так, 
+чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris" """
 
 name = "vasya petya kolya dima maksim anton makar"
 print(name.title())
-
