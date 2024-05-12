1d0d32905fd2dd8389f9f462a3a567aaafca369d
Linard <linardminsk@gmail,com>
2024-05-12 19:26:25 +0300

    commit #24

--- homework5.py
+++ homework5.py
@@ -13,16 +13,16 @@ while S == "":
 print(S + "ing")
 
 # В строке “Ivanou Ivan” поменяйте естами слова => "Ivan Ivanou"
-Name_str = "Ivanou Ivan"
-Name_list = Name_str.split(" ")
-print(Name_list[1], Name_list[0])
+NAME_STR = "Ivanou Ivan"
+NAME_LIST = NAME_STR.split(" ")
+print(NAME_LIST[1], NAME_LIST[0])
 
 # Напишите программу которая удаляет пробел в начале, в конце строки
-text = " multiple statements found "
-print(text.lstrip().rstrip())
+TEXT = " multiple statements found "
+print(TEXT.lstrip().rstrip())
 
 # Имена собственные всегда начинаются с заглавной буквы,
 # за которой следуют строчные буквы. Исправьте данное имя собственное так,
 # чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris"
-name = "vasya petya kolya dima maksim anton makar"
-print(name.title())
+NAME = "vasya petya kolya dima maksim anton makar"
+print(NAME.title())
